760631bbc5cf41eec77dc34e6d0dc6bc839cf23f
ALESSANDRA ULINICK <aleeulinick@gmail.com>
2023-03-21 19:04:50 -0300

    alo

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-416gox_%c(k@#axbyoch0$k!dsdrbw0o6_&zuuvw9i2(01s5y+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
