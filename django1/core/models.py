46a7aed6800219db5463252b0b6209865932b1e2
ALESSANDRA ULINICK <aleeulinick@gmail.com>
2023-03-21 09:47:06 -0300

    Adicionando migrations no projeto

--- django1/core/models.py
+++ django1/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    estoque = models.IntegerField('Quantidade em Estoque')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('Email', max_length=100)
--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'django1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['core/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
